4333332abf17da8fe5f2b32dbddc35a2ba744b9e
Jaragan Ittah <support@tesmon.io>
2023-04-11 14:06:37 -0700

    Use asynchronous, non-blocking HTTP client
    Improved performance and scalability by switching to an async client for API requests.

--- src/main/java/io/tesmon/TesmonEventClient.java
+++ src/main/java/io/tesmon/TesmonEventClient.java
@@ -2,15 +2,18 @@ package io.tesmon;
 
 import org.json.JSONObject;
 
+import java.util.concurrent.CompletableFuture;
+
 /**
  * This interface provides methods to send events to Tesmon.
  */
 public interface TesmonEventClient {
   /**
    * Sends an event to Tesmon.
-   * @param eventKey Unique event key to use for the event.
+   *
+   * @param eventKey  Unique event key to use for the event.
    * @param eventBody A JSON object representing the body of the event.
    * @return A JSON string containing the "eventId" field with the ID of the created event, or a "message" field with the error message if the event could not be created or sent.
    */
-  String sendEvent(String eventKey, JSONObject eventBody);
+  CompletableFuture<String> sendEvent(String eventKey, JSONObject eventBody);
 }
