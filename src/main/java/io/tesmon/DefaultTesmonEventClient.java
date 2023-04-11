2cdcea533d2c9d69235300393f41dd7b02fde30d
Jaragan Ittah <support@tesmon.io>
2023-04-11 14:06:37 -0700

    Use asynchronous, non-blocking HTTP client
    Improved performance and scalability by switching to an async client for API requests.

--- src/main/java/io/tesmon/DefaultTesmonEventClient.java
+++ src/main/java/io/tesmon/DefaultTesmonEventClient.java
@@ -2,17 +2,22 @@ package io.tesmon;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.concurrent.CompletableFuture;
 
 public class DefaultTesmonEventClient implements TesmonEventClient {
+  private final static int DEFAULT_TIMEOUT = 5000;
   private String tesmonTreBaseUrl;
 
   public DefaultTesmonEventClient(String tesmonTreBaseUrl) {
@@ -24,36 +29,68 @@ public class DefaultTesmonEventClient implements TesmonEventClient {
   }
 
   @Override
-  public String sendEvent(String eventKey, JSONObject eventBody) {
+  public CompletableFuture<String> sendEvent(String eventKey, JSONObject eventBody) {
     if (tesmonTreBaseUrl == null || tesmonTreBaseUrl.isEmpty()) {
       throw new IllegalArgumentException("Tesmon Test Run Engine base URL is null or empty");
     }
     String url = tesmonTreBaseUrl + "/v1/events";
 
-    CloseableHttpClient httpClient = HttpClients.createDefault();
-    HttpPost httpPost = new HttpPost(url);
+    CloseableHttpAsyncClient httpClient = HttpAsyncClients.custom()
+        .setDefaultRequestConfig(RequestConfig.custom()
+            .setConnectionRequestTimeout(DEFAULT_TIMEOUT)
+            .setConnectTimeout(DEFAULT_TIMEOUT)
+            .setSocketTimeout(DEFAULT_TIMEOUT)
+            .build())
+        .build();
 
-    JSONObject event = new JSONObject();
-    event.append("eventKey", eventKey);
-    event.append("eventBody", eventBody);
-    event.append("createdAt", System.currentTimeMillis());
-
-    StringEntity payloadEntity = new StringEntity(event.toString(), ContentType.APPLICATION_JSON);
-    httpPost.setEntity(payloadEntity);
+    CompletableFuture<String> future = new CompletableFuture<>();
 
     try {
-      HttpResponse httpResponse = httpClient.execute(httpPost);
-      int statusCode = httpResponse.getStatusLine().getStatusCode();
+      httpClient.start();
 
-      String responseBody = EntityUtils.toString(httpResponse.getEntity());
-      if (statusCode != HttpStatus.SC_OK) {
-        System.err.println("Failed to send event. HTTP status code: " + statusCode);
-      }
+      HttpPost httpPost = new HttpPost(url);
 
-      return responseBody;
-    } catch (IOException e) {
+      JSONObject event = new JSONObject();
+      event.append("eventKey", eventKey);
+      event.append("eventBody", eventBody);
+      event.append("createdAt", System.currentTimeMillis());
+
+      StringEntity payloadEntity = new StringEntity(event.toString(), ContentType.APPLICATION_JSON.withCharset(Charset.forName("UTF-8")));
+      httpPost.setEntity(payloadEntity);
+
+      httpClient.execute(httpPost, new FutureCallback<>() {
+        @Override
+        public void completed(HttpResponse result) {
+          try {
+            int statusCode = result.getStatusLine().getStatusCode();
+            String responseBody = EntityUtils.toString(result.getEntity());
+
+            if (statusCode != HttpStatus.SC_OK) {
+              System.err.println("Failed to send event. HTTP status code: " + statusCode + ", body: " + responseBody);
+            }
+
+            future.complete(responseBody);
+          } catch (IOException e) {
+            future.completeExceptionally(e);
+          }
+        }
+
+        @Override
+        public void failed(Exception ex) {
+          future.completeExceptionally(ex);
+        }
+
+        @Override
+        public void cancelled() {
+          future.cancel(true);
+        }
+      });
+    } catch (Exception e) {
       System.err.println("Failed to send event. Exception: " + e.getMessage());
-      return null;
+      future.completeExceptionally(e);
     }
+
+    return future;
   }
 }
+
